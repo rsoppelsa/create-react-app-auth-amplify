e53f2fdce3ef3e32f7da862303b383a5681138f4
rsoppelsa <richard@webess.co.uk>
2020-04-29 18:22:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Auth } from 'aws-amplify';
+
+Auth.currentAuthenticatedUser({
+    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+}).then(user => console.log(user))
+.catch(err => console.log(err));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
